52c41137c6803ac17ab29c1b77bfa4adf3fd1251
Ethan Tsai <ethan.tsai95@gmail.com>
2023-02-18 18:07:42 -0800

    added smoothing and normalized to first two ELFIN channels instead of just first

--- ducting_analysis_elfin.jl
+++ ducting_analysis_elfin.jl
@@ -19,18 +19,18 @@ folder = "run19/"
 # case = "ELA_NA_210112T0226_3_250000", done
 # case = "ELA_ND_200904T0112_3_250000", done
 # case = "ELB_ND_200926T0101_3_250000", done
-case = "ELA_SD_210203T0902_3_250000"
+# case = "ELA_SD_210203T0902_3_250000", done (took 10 hrs)
 # case = "ELA_SD_210203T1342_3_250000"
 # case = "ELA_NA_210203T2046_3_250000"
 # case = "ELA_NA_210203T2047_3_250000", done
-# case = "ELA_SD_211001T0501_3_250000"
+# case = "ELA_SD_211001T0501_3_250000", done (took 10 hrs)
 # case = "ELA_SD_211001T0810_3_250000"
 # case = "ELA_SA_211101T0424_3_250000"
 # case = "ELA_SA_211102T2218_3_250000"
 
-@time @load save_dir*folder*case*".jld2" sol;
-rm = sol2rm(sol, case);
-@time @save "result_matrix/"*case[1:18]*".jld2" rm
+# @time @load save_dir*folder*case*".jld2" sol;
+# rm = sol2rm(sol, case);
+# @time @save "result_matrix/"*case[1:18]*".jld2" rm
 
 
 
@@ -45,13 +45,13 @@ stop = DateTime(2021,1,6,11,54,1)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.3
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -67,13 +67,13 @@ stop = DateTime(2021,1,8,6,46,56)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = .7
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -89,13 +89,13 @@ stop = DateTime(2021,1,11,17,50,58)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.4
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -111,12 +111,13 @@ stop = DateTime(2021,1,12,2,26,38)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-plot(E_bins, normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -132,13 +133,13 @@ stop = DateTime(2020,9,4,1,12,32)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.3
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -154,13 +155,13 @@ stop = DateTime(2020,9,26,1,1,20)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.3
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -176,13 +177,13 @@ stop = DateTime(2021,2,3,9,1,56)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -198,13 +199,13 @@ stop = DateTime(2021,2,3,13,42,32)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -220,13 +221,13 @@ stop = DateTime(2021,2,3,20,46,48)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario);
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -242,13 +243,13 @@ stop = DateTime(2021,2,3,20,47,33)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario)
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 0.9
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -257,20 +258,20 @@ savefig(plot2, "images/"*scenario*"_ratiocomparison.png")
 
 
 ############
-scenario = "ELA_SD_211001T0501"
+scenario = "ELA_SD_211001T0459"
 start = DateTime(2021,10,1,4,59,57)
 stop = DateTime(2021,10,1,5,0,18)
 ############
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario)
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -286,13 +287,13 @@ stop = DateTime(2021,10,1,8,10,13)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario)
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -308,13 +309,13 @@ stop = DateTime(2021,11,1,4,23,44)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario)
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
@@ -330,13 +331,13 @@ stop = DateTime(2021,11,2,22,18,35)
 @time @load "result_matrix/"*scenario*".jld2" rm
 id, yy, mm, dd, HH, MM = extract_idyymmddHHMM(scenario)
 sim_ratio = prec_to_trap_ratio(rm)
+sim_ratio_sm = smooth(sim_ratio[1], 6, 5)
 elfin_p2t, elfin_p2t_error = extract_elfin_p2t_ratio(
     mm*dd*yy*"_"*HH, start, stop
     )
 @info "Model E_max = $(elfin_p2t[1][last_index_before_zero(elfin_p2t[2])]) keV for scenario $scenario"
-normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio[1])
-multiplier = 1.0
-plot(E_bins, multiplier*normalizer*sim_ratio[1], label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
+normalizer = normalize_to_elfin(elfin_p2t[2], sim_ratio_sm)
+plot(E_bins, normalizer*sim_ratio_sm, label=scenario[9:end]*"_model", color = c1, marker = stroke(3,c1), linewidth=4, markersize = 3)
 plot!(xscale=:log10, yscale=:log10, xlim=(52,1000), ylim=(1e-2, 15))
 plot!(elfin_p2t, yerror=elfin_p2t_error, color = c5, marker = stroke(3,c5), linewidth=4, markersize = 3, label="ELFIN-"*id*" "*mm*"/"*dd*" "*HH*":"*MM)
 plot!(title = mm*"/"*dd*" "*HH*":"*MM*" prec/trap flux ratio comparison", ylabel="j_parallel/j_perp", xlabel = "Energy (keV)")    
